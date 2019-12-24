2d1c1512393cc8b8f7159dd9e16030a2524be197
luis carlos <luisestrela.bol951@gmail.com>
2019-12-24 14:35:30 -0300

    using local stroge to save user id

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import logo from './assets/logo.svg'
+import api from './services/api'
 
 function App() {
+  const [ email, setEmail ] = useState('');//armazena o email
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const response = await api.post('/session', {email});
+    const{ _id } = response.data.user;
+
+    localStorage.setItem('user', _id)
+  }
+
   return (
     <div className="container">
       <img src={logo} />     
@@ -13,12 +24,20 @@ function App() {
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa 
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-mail *</label>
-          <input type="email" id="email" placeholder="Digite seu e-mail..."/>
+
+          <input 
+            type="email" 
+            id="email" 
+            placeholder="Digite seu e-mail..." 
+            className="input"
+            value={email}
+            onChange={event =>setEmail(event.target.value)}
+          />
 
 
-          <button type="submit">Entrar</button>
+          <button className="btn" type="submit">Entrar</button>
         </form>
       </div>
     </div>
